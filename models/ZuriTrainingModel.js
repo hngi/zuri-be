419ccee8deeceaceeb4089872180e68b69ba46cc
deeone2 <del2luv@gmail.com>
2020-08-07 11:01:13 +0100

    zuri training model modifications

--- models/ZuriTrainingModel.js
+++ models/ZuriTrainingModel.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const { InternObject } = require('./Intern');
 
 const { Schema } = mongoose;
 
 const zuriTrainingSchema = Schema({
-  ...InternObject
+  fullName: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  course: {
+    type: String,
+    required: true
+  },
+  track: {
+    type: String,
+    enum: ['frontend', 'mobile', 'design', 'backend'],
+    required: true
+  },
+
+  level: {
+    type: String,
+    enum: ['beginner', 'intermediate'],
+    default: 'beginner'
+  }
 },
 {
   timestamps: true
